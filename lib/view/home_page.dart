351b2b0ff89673efd82ff66c3d1f9dba4864824b
Sanjib Sinha <sanjib12sinha@gmail.com>
2021-11-29 09:27:48 +0530

    fourth commit to blog page

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -1,8 +1,8 @@
-import 'package:build_blog_with_flutter/model/blog_post.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 import '/model/global_theme.dart';
+import '/model/blog_post.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -32,7 +32,6 @@ class HomeBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final blogPosts = Provider.of<List<BlogPost>>(context);
     return Center(
       child: ListView(
         children: [
@@ -49,11 +48,139 @@ class HomeBody extends StatelessWidget {
             padding: const EdgeInsets.all(5),
             child: Text(
               'I am a clumsy coder who neither can write decent code, nor '
-              'can write fiction. But I try to be a good human being.',
+              'can write fiction.'
+              ' That friction disturbs me a lot. '
+              'But I try to be a good human being.',
               style: homeTheme.textTheme.headline2,
             ),
           ),
+          const BlogPostHome(),
         ],
+      ),
+    );
+  }
+}
+
+class BlogPostHome extends StatelessWidget {
+  const BlogPostHome({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final blogPosts = Provider.of<List<BlogPost>>(context);
+    return Column(
+      children: blogPosts
+          .map(
+            (e) => BlogPostOutput(
+              id: e.id,
+              title: e.title,
+              date: e.date,
+              content: e.content,
+            ),
+          )
+          .toList(),
+    );
+  }
+}
+
+class BlogPostOutput extends StatelessWidget {
+  final String id;
+  final String title;
+  final DateTime date;
+  final String content;
+
+  const BlogPostOutput({
+    Key? key,
+    required this.id,
+    required this.title,
+    required this.date,
+    required this.content,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        InkWell(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => BlogDetail(
+                  id: id,
+                  title: title,
+                  date: date,
+                  content: content,
+                ),
+              ),
+            );
+          },
+          child: Text(
+            title,
+            style: const TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class BlogDetail extends StatelessWidget {
+  final String id;
+  final String title;
+  final DateTime date;
+  final String content;
+  const BlogDetail({
+    Key? key,
+    required this.id,
+    required this.title,
+    required this.date,
+    required this.content,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final blogTheme = Provider.of<GlobalTheme>(context).globalTheme;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Blog Page',
+          style: blogTheme.appBarTheme.titleTextStyle,
+        ),
+      ),
+      body: Center(
+        child: ListView(
+          children: [
+            Container(
+              margin: const EdgeInsets.all(5),
+              padding: const EdgeInsets.all(5),
+              child: Text(
+                title,
+                textAlign: TextAlign.left,
+                style: blogTheme.textTheme.headline1,
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.all(5),
+              padding: const EdgeInsets.all(5),
+              child: Text(
+                content,
+                textAlign: TextAlign.left,
+                style: blogTheme.textTheme.bodyText1,
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.all(5),
+              padding: const EdgeInsets.all(5),
+              child: Text(
+                date.toString(),
+                style: blogTheme.textTheme.bodyText1,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
